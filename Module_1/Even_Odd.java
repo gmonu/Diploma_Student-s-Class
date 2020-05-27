fd109935887afb99d24250ca8c68ae5e56eac98f
Monu Gupta <42869855+gmonu@users.noreply.github.com>
2020-05-27 13:30:21 +0530

    Please update

--- Module_1/Even_Odd.java
+++ Module_1/Even_Odd.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Even_Odd {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		sc.close();
+	public static void main(String[] args) 
+	{
+	
+		//Paste your program here
 	}
 
 }
